3231813dc999e32e9b5487bd31c6d874a3f9b078
Derek Sneddon <dsneddon00@gmail.com>
2021-11-03 17:49:09 -0600

    framework for assignment

---
+++ main.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import math
+import random
+
+class RSA:
+
+    def __init__(self):
+        return
+
+    def GenerateKeys(longOne, longTwo):
+        return
+
+    def Encrypt(inputFileName, outputFileName):
+        return
+
+    def Decrypt(inputFileName, outputFileName):
+        return
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
